415403206e7b0fbe6b263c1977c7bef1c08c22b9
Vivian Conrad <44071734+vivianconrad@users.noreply.github.com>
2023-07-27 14:23:04 -0500

    Create TodoList.js

---
+++ react/todoList/TodoList.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() === '') return;
+    setTasks([...tasks, newTask]);
+    setNewTask('');
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Enter a new task"
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <p>Total tasks: {tasks.length}</p>
+    </div>
+  );
+};
+
+export default TodoList;
